33cc7d0b8710682c0d9fa05da3d857d2190f581b
PJ Brindisi <npj@frestyl.com>
2011-05-26 18:23:40 +0200

    deserialization methods accessible at module level, as advertised

--- lib/queryalize.rb
+++ lib/queryalize.rb
@@ -4,4 +4,16 @@ module Queryalize
   def self.new(klass)
     SerializableQuery.new(klass)
   end
+  
+  def self.from_hash(hash)
+    SerializableQuery.from_hash(hash)
+  end
+  
+  def self.from_json(json)
+    SerializableQuery.from_json(json)
+  end
+  
+  def self.from_yaml(yaml)
+    SerializableQuery.from_yaml(yaml)
+  end
 end
